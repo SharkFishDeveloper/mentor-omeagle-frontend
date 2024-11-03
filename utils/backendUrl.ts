6ceaf271541bff3803e05fef3bb4feb590ab3454
Shahzeb Akhtar <shahzebakhtar66@gmail.com>
2024-11-03 22:19:56 +0530

    url backend

--- utils/backendUrl.ts
+++ utils/backendUrl.ts
@@ -1,2 +1,2 @@
-export  const  BACKEND_URL:string = "http://localhost:3000";
+export  const  BACKEND_URL:string = "https://omeagle-backend.onrender.com";
 export const univerOptions = ["Thapar","Harward","MIT","IIT-B","IIT-D","IIT-K","Stanford"];
